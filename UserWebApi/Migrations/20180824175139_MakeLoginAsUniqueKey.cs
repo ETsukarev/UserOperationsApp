c024b7cf691b5ed887b2a25553ca90e85b4d1183
ted <tsukarev.ed@gmail.com>
2018-08-26 23:20:42 +0300

    Made login as unique key
    Made login as unique key.

---
+++ UserWebApi/Migrations/20180824175139_MakeLoginAsUniqueKey.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserWebApi.Migrations
+{
+    public partial class MakeLoginAsUniqueKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(name: "Login", table: "Users", type: "nvarchar(20)");
+            migrationBuilder.AddUniqueConstraint("FieldLoginUniqueConstraint", "Users", "Login");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint("FieldLoginUniqueConstraint", "Users");
+            migrationBuilder.AlterColumn<string>(name: "Login", table: "Users", type: "nvarchar(max)");
+        }
+    }
+}
